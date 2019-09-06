c33f560e96076fab15575438561832d9f6e41bfa
Sinan Jasar <sinanjasar@live.dk>
2019-09-06 12:40:30 +0200

    added front end to /all endpoint

--- src/main/webapp/js.js
+++ src/main/webapp/js.js
@@ -6,18 +6,25 @@
 
 var div = document.getElementById("div");
 let url = "https://sinanjasar.dk/MovieRest/api/cinema/all";
-    fetch(url)
-            .then(res => res.json()) //in flow1, just do it
-            .then(data => {
-                // Inside this callback, and only here, the response data is available
-                console.log("data", data);
-                var head = "<tr><th>Name</th><th>Phone</th></tr>";
-                data = data.map(obj => "<tr><td>" + obj.name + "</td><td>" + obj.phone + "</td></tr>");
-                div.innerHTML = "<table>" + head.concat(data) + "</table>";
-
-                // data.map(property => "<td>"+property+"</td>");
-                /* data now contains the response, converted to JavaScript
-                 Observe the output from the log-output above
-                 Now, just build your DOM changes using the data*/
+fetch(url)
+        .then(res => res.json()) //in flow1, just do it
+        .then(data => {
+            // Inside this callback, and only here, the response data is available
+            console.log("data", data);
+            var head = "<tr><th>Name</th><th>Release</th><th>Actors</th></tr>";
+            var td = data.map(obj => {
+                var str = "";
+                for (var p in obj) {
+                    str += "<td>" + obj[p] + "</td>" + "\r";
+                }
+                return "<tr>" + str + "</tr>";
             });
+            data = td.join('');
+            div.innerHTML = "<table class=\"table-striped\">" + head.concat(data) + "</table>";
+
+            // data.map(property => "<td>"+property+"</td>");
+            /* data now contains the response, converted to JavaScript
+             Observe the output from the log-output above
+             Now, just build your DOM changes using the data*/
+        });
         
